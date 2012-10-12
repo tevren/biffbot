bf547442fb7b0201e70491762203e05ee9f15fab
Anurag Mohanty <anurag@columbia.edu>
2012-10-12 12:12:17 -0400

    fixed raise left in by accident

--- lib/biffbot/base.rb
+++ lib/biffbot/base.rb
@@ -8,13 +8,11 @@ module Biffbot
 			@url = url
 			output = Hash.new
 			request = "http://www.diffbot.com/api/article?token=#{@token}&url=#{@url}"
-			raise request.to_s
 			options.each_pair do |key,value|
 				if key.match(/html|dontStripAds|tags|comments|summary|stats/) && value == true
 					request = request + "&#{key}"
 				end
 			end
-			puts request
 			response = HTTParty.get(request)
 			response.parsed_response.each_pair do |key,value|
 				output[key.to_sym] = value
