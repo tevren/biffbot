8b5c8ae70db5de9e3a6a60bb264478d09c6ed118
Anurag Mohanty <amohanty@amplify.com>
2013-05-11 22:36:53 -0400

    declare urls as an array, otherwise method assumes 2nd url is part of options

--- lib/biffbot/base.rb
+++ lib/biffbot/base.rb
@@ -21,20 +21,20 @@ module Biffbot
 			end
 			return output
 		end
-		def batch(urls, options={})
+		def batch(urls=[],options={})
 			output = []
 			request = "http://www.diffbot.com/api/batch"
-      batch = urls.map do |url|
-        { :method => "GET", :relative_url => "/api/article?token=#{@token}&url=#{url}" }
-      end
-      options = { :body => {:token => @token, :batch => batch.to_json }, :basic_auth => @auth }
+      		batch = urls.map do |url|
+    			{ :method => "GET", :relative_url => "/api/article?token=#{@token}&url=#{url}" }
+      		end
+     		options = { :body => {:token => @token, :batch => batch.to_json }, :basic_auth => @auth }
 			response = HTTParty.post(request, options)
       
 			JSON.parse(response.parsed_response).each do |response_dict|
-        puts response_dict.inspect
+        		puts response_dict.inspect
 				output << JSON.parse(response_dict["body"])
 			end
 			return output
-    end
+    	end
 	end
 end
